e22a2b0503f2186f9058533efded34584b41c448
Santiago Rada <santynrada@gmail.com>
2024-10-31 12:51:42 -0300

    Clase 12

---
+++ JavaScript/Clase-12/javascript/clase-12.js
@@ -0,0 +1,6 @@
+// LET = Palabra reservada para crear variables
+// 'nombre' Este es el nombre de la variable
+// 'Juan Pablo' Es el valor que se le asigna a la variable 'nombre'
+let nombre = "Juan Pablo";
+
+console.log(nombre);
